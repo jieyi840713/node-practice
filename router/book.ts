102905dc929a8394637f10804d6d567da44fbb9d
JayLee <jieyi840713@gmail.com>
2023-07-18 15:57:35 +0800

    錯誤處理及同步概念

--- router/book.ts
+++ router/book.ts
@@ -14,6 +14,13 @@ router.get("/page", (req, res) => {
 
 router.get("/data", (req, res) => {
   fs.readFile("data.json", "utf8", (err, data) => {
+    // err -> 錯誤資料 ， data -> 讀取資料
+    // 若有錯誤，通常沒事err 為unddfined
+    if (err) {
+      console.log(err);
+      return res.send("檔案有問題");
+    }
+
     console.log(data);
     console.log(typeof data); // 檢查資料型別
     const result = JSON.parse(data); // 將資料轉成JSON
@@ -22,6 +29,12 @@ router.get("/data", (req, res) => {
     res.json(result); // 回傳前端JSON資料
     // res.send(data); // 回傳前端string資料
   });
+});
+
+router.get("/data-2", (req, res) => {
+  const data2 = fs.readFile("data.json", "utf8", () => {}); // 會得到undefined
+  console.log(data2);
+  res.send(data2);
 });
 
 // [module][1]講router導出，等著別人require引入使用
